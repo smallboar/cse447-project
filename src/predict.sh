641003d4ff34afeb5dcfb1863234c41277f802ed
smallboar <williampfang@gmail.com>
2026-01-28 01:24:29 -0800

    Misc changes to complete checkpoint 1

--- src/predict.sh
+++ src/predict.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set -v
-python src/myprogram.py test --work_dir work --test_data $1 --test_output $2
+python3 src/myprogram.py test --work_dir work --test_data $1 --test_output $2
